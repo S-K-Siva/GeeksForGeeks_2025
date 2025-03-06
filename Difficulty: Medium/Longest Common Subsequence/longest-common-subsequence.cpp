0f0abe4e162507d65e5ec4669a205a7419f6d1ed
Siva Sakthivel <88235149+S-K-Siva@users.noreply.github.com>
2025-03-06 15:42:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.cpp
+++ Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.cpp
@@ -11,7 +11,9 @@ class Solution {
         int m = s1.size();
         int n = s2.size();
         vector<vector<int>> dp(m+1,vector<int>(n+1,0));
-        for(int i = 1;i<=m;i++){
+        
+        for(int i = 1;i<=m;i++)
+        {
             for(int j = 1;j<=n;j++){
                 if(s1[i-1] == s2[j-1]){
                     dp[i][j] = 1 + dp[i-1][j-1];
@@ -20,6 +22,7 @@ class Solution {
                 }
             }
         }
+        
         return dp[m][n];
     }
 };
