d7d84ca4357b60fa9c630e0aa3682931a5dd12b0
Siva Sakthivel <88235149+S-K-Siva@users.noreply.github.com>
2025-03-21 16:03:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -4,90 +4,41 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-  
-    bool bfs(vector<bool>& vis, vector<vector<int>>& adj, int start){
-        
+    // Function to detect cycle in an undirected graph.
+    bool bfs(int start,vector<vector<int>> adj, 
+    vector<int>& vis){
         queue<pair<int,int>> que;
         que.push({-1,start});
         
         while(!que.empty()){
-            pair<int,int> current = que.front();
+            int parent = que.front().first;
+            int node = que.front().second;
+            vis[node] = 1;
             que.pop();
-            int parent = current.first;
-            int node = current.second;
-            vis[node] = true;
             for(auto neigh : adj[node]){
                 if(neigh == parent) continue;
-                if(vis[neigh]){
-                    return true;
-                }
-                if(!vis[neigh]) que.push({node, neigh});
+                if(vis[neigh]) return true;
+                if(!vis[neigh]) que.push({node,neigh});
             }
         }
         return false;
-        
     }
-    
-    bool dfs(vector<bool>& vis, vector<vector<int>>& adj, int node, int parent){
-        vis[node] = true;
-        
-        for(auto neigh : adj[node]){
-            if(neigh == parent) continue;
-            if(vis[neigh]) return true;
-            if(!vis[neigh]){
-                if(dfs(vis,adj,neigh, node) == true) return true;
-            }
-        }
-        
-        return false;
-    }
-    
-    int findParent(vector<int>&parent, int node){
-        if(parent[node] == node) return node;
-        return parent[node] = findParent(parent,parent[node]);
-    }
-    bool findByRank(vector<int>&parent,vector<int>&rank, int u,int v){
-        int ultimateParentOfU = findParent(parent,u);
-        int ultimateParentOfV = findParent(parent,v);
-        if(ultimateParentOfU == ultimateParentOfV) return true;
-        if(rank[ultimateParentOfU] < rank[ultimateParentOfV]){
-            parent[ultimateParentOfU] = ultimateParentOfV;
-        }else if(rank[ultimateParentOfU] > rank[ultimateParentOfV]){
-            parent[ultimateParentOfV] = ultimateParentOfU;
-        }else{
-            parent[ultimateParentOfV] = ultimateParentOfU;
-            ++rank[ultimateParentOfU];
-        }
-        return false;
-    }
-    // Function to detect cycle in an undirected graph.
     bool isCycle(vector<vector<int>>& adj) {
-        
         int n = adj.size();
-        vector<bool> visited(n,false);
-        vector<int> parent(n,-1);
-        vector<int> rank(n,0);
-        // for(int i = 0;i<n;i++){
-        //     if(!visited[i]){
-        //         // if(bfs(visited, adj, i) == true) return true;
-        //         // if(dfs(visited, adj, i, -1) == true) return true;
-                
-        //     }
-        // }
-        for(int i = 0;i<n;i++)
-        {
-            parent[i] = i;
-        }
+        vector<int> vis(n,0);
+      
         for(int i = 0;i<n;i++){
-            for(auto neigh : adj[i]){
-                if(i < neigh && findByRank(parent,rank,i,neigh) == true) return true; 
+            if(!vis[i]){
+                if(bfs(i,adj,vis) == true) return true;
             }
         }
         return false;
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
