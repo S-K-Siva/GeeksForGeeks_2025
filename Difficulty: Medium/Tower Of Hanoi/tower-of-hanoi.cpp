cf0554fc9ac17a76b12a6fea61bfc7e06a92e728
Siva Sakthivel <88235149+S-K-Siva@users.noreply.github.com>
2025-04-22 18:07:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.cpp
+++ Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.cpp
@@ -10,16 +10,10 @@ class Solution {
   public:
     // You need to complete this function
 
-    int towerOfHanoi(int n, int source, int destination,
-    int auxilary) {
-        if(n == 0) return 0;
-        if(n == 1){
-            return 1;
-        }
-        int move1 = towerOfHanoi(n-1,source,auxilary,destination);
-        int move2 = towerOfHanoi(n-1,auxilary,destination,source);
-        return move1 + move2 + 1;
+    int towerOfHanoi(int n, int from, int to, int aux) {
+        if(n == 0 || n == 1) return n;
         
+        return 1 + towerOfHanoi(n-1,from, to, aux) + towerOfHanoi(n-1,aux,to,from);
     }
 };
 
