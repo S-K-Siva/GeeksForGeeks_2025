c91d54034008b321ec2558ed2bacd560f61c47bb
Siva Sakthivel <88235149+S-K-Siva@users.noreply.github.com>
2025-09-07 14:43:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Permutations of a String/permutations-of-a-string.cpp
+++ Difficulty: Medium/Permutations of a String/permutations-of-a-string.cpp
@@ -1,44 +1,13 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    vector<string> findPermutation(string &S) {
-        sort(S.begin(),S.end());
-		    set<string> ss;
-		    do{
-		        ss.insert(S);
-		    }while(next_permutation(S.begin(),S.end()));
-		    
-		    vector<string> res(ss.begin(),ss.end());
-		    
-		    return res;
+    vector<string> findPermutation(string &s) {
+        set<string> result;
+        sort(s.begin(),s.end());
+        do{
+            result.insert(s);
+        }while(next_permutation(s.begin(),s.end()));
+        
+        vector<string>ss(result.begin(),result.end());
+        return ss;
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string S;
-        cin >> S;
-        Solution ob;
-        vector<string> ans = ob.findPermutation(S);
-        sort(ans.begin(), ans.end());
-        for (auto i : ans) {
-            cout << i << " ";
-        }
-        cout << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
